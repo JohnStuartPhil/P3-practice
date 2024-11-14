0d4b05252673035c021c1d3208903e83075ac26c
John Stuart Phillips <0q5av15l@students.codeinstitute.net>
2024-11-14 22:37:35 +0000

    test5

--- run.py
+++ run.py
@@ -15,3 +15,23 @@
 #answer = "Paris"
 #collective = ("It is " + answer)
 #print(collective)
+
+#From String Methods
+#my_string = "HELLO WORLD"
+#greetings = my_string.replace("WORLD", "Dave")
+#print(greetings)
+
+question = "What is the Capital of France?"
+print(question)
+print()
+a = "Is it A: London,"
+b = "B: Paris"
+c = "or C: Moscow"
+print(a, b, c)
+print()
+given_answer = input("Please give select a letter: ")
+
+#if letter == b:
+    #print(f"you answered {letter}, that is correct")
+#else:
+    #print(f"You answered {letter}, that is incorrect")
