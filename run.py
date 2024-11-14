8b921f6c17ffe40d844a9afcad3f150f1fb9c578
John Stuart Phillips <0q5av15l@students.codeinstitute.net>
2024-11-14 22:13:24 +0000

    test4

--- run.py
+++ run.py
@@ -4,9 +4,14 @@
 #answer = "The capital of France is " + capital
 #print(answer)
 
-question = input("What is the capital of France? ")
-print(f"You answered: {question}")
-answer = 'Paris'
-print()
-print(f"The correct answer is: {answer}")
+#question = input("What is the capital of France? ")
+#print(f"You answered: {question}")
+#answer = 'Paris'
+#print()
+#print(f"The correct answer is: {answer}")
 
+#question = input("What is the capital of France? ")
+#print(question)
+#answer = "Paris"
+#collective = ("It is " + answer)
+#print(collective)
