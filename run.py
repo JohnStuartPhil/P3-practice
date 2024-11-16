6ee2a33d42c4ad32dd4727e7ce04ab0cee98df1d
John Stuart Phillips <0q5av15l@students.codeinstitute.net>
2024-11-16 20:50:10 +0000

    test19

--- run.py
+++ run.py
@@ -28,7 +28,9 @@ def the_question():
     question = "What is the Capital of the United States?"
     print(question)
     print()
+
 the_question()
+
 def the_options():
     options = {
         "A": "Los Angeles",
